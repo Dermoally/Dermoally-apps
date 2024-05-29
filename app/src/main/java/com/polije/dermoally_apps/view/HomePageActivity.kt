6dbb133d17a7f9c579d208198cdcaa43b54ecc34
Fatiya Quzza <fatiya_qu@mhs.unsyiah.ac.id>
2024-05-30 01:49:22 +0700

    make fragment

--- app/src/main/java/com/polije/dermoally_apps/view/HomePageActivity.kt
+++ app/src/main/java/com/polije/dermoally_apps/view/HomePageActivity.kt
@@ -6,8 +6,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.polije.dermoally_apps.R
 import com.polije.dermoally_apps.databinding.ActivityHomePageBinding
+import com.polije.dermoally_apps.fragment.FavoriteFragment
+import com.polije.dermoally_apps.fragment.HistoryFragment
+import com.polije.dermoally_apps.fragment.HomeFragment
+import com.polije.dermoally_apps.fragment.SettingFragment
 
 class HomePageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomePageBinding
@@ -22,6 +27,25 @@ class HomePageActivity : AppCompatActivity() {
         binding.bottomNavView.menu.getItem(2).isEnabled = false
         binding.floatingButton.setColorFilter(Color.parseColor("#f6f3ee"))
 
+        replaceFragment(HomeFragment())
 
+        binding.bottomNavView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> replaceFragment(HomeFragment())
+                R.id.ic_setting -> replaceFragment(SettingFragment())
+                R.id.ic_fav -> replaceFragment(FavoriteFragment())
+                R.id.ic_history -> replaceFragment(HistoryFragment())
+
+            }
+            true
+        }
+    }
+
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameFragment, fragment)
+        fragmentTransaction.commit()
     }
 }
