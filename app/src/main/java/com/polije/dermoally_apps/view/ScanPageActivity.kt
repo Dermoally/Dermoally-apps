e52c9068e1c20c8e6de03b0fcc82692657ca48f9
Fatiya Quzza <fatiya_qu@mhs.unsyiah.ac.id>
2024-06-01 23:00:42 +0700

    make ui result and scan

---
+++ app/src/main/java/com/polije/dermoally_apps/view/ScanPageActivity.kt
@@ -0,0 +1,24 @@
+package com.polije.dermoally_apps.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.polije.dermoally_apps.R
+import com.polije.dermoally_apps.databinding.ActivityHomePageBinding
+import com.polije.dermoally_apps.databinding.ActivityScanPageBinding
+
+class ScanPageActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityScanPageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityScanPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgScan.setBackgroundResource(R.drawable.custom_transparant);
+        binding.imgScan.setClipToOutline(true);
+
+    }
+}
