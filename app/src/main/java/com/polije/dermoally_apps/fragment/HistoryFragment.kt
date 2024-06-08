a29d3ef3177a8b3ca7664804c74884f27774420a
fawaidach <fwdachmd@gmail.com>
2024-06-08 21:55:40 +0700

    update search

--- app/src/main/java/com/polije/dermoally_apps/fragment/HistoryFragment.kt
+++ app/src/main/java/com/polije/dermoally_apps/fragment/HistoryFragment.kt
@@ -1,10 +1,14 @@
 package com.polije.dermoally_apps.fragment
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.polije.dermoally_apps.R
 import com.polije.dermoally_apps.adapter.HistoryAdapter
@@ -27,6 +31,16 @@ class HistoryFragment : Fragment() {
         historyAdapter = HistoryAdapter(dummyHistories)
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
         binding.recyclerView.adapter = historyAdapter
+        val face: Typeface? = ResourcesCompat.getFont(requireContext(), R.font.poppinsregular)
+        val searchText = binding.searchView.findViewById<View>(androidx.appcompat.R.id.search_src_text) as TextView
+        searchText.typeface = face
+        searchText.textSize = 12f
+        searchText.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.black))
+
         return root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
